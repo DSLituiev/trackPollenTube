c81c4c2f85316e6eb50c314518150de2fdbb1571
Dmytro S Lituiev <d.lituiev@gmail.com>
2015-03-24 17:32:38 +0100

    another heuristic (min_speed_boundaries) added + optimization procedure

--- matlab_source/bendings.m
+++ matlab_source/bendings.m
@@ -8,7 +8,7 @@ function [ bnd, dx, ddx ] = bendings( varargin )
 % Input:
 % ======
 % - x    -- data points (non-decreasinf track/path)
-% - R    -- radius of the binomial filter used to smooth before taking the derivative 
+% - R    -- radius of the binomial filter used to smooth before taking the derivative
 %           (size = 2*R + 1)
 % - THR  -- threshold for removing redundant bending points
 %           the raw bending points are discarded if
@@ -22,13 +22,14 @@ p.KeepUnmatched = true;
 addRequired(p, 'x', @(x)( isnumeric(x) && sum(size(x)>1)==1 ) );
 addParamValue(p, 'visualize',  false, @isscalar);
 addParamValue(p, 'R',  3, @isscalar);
-addParamValue(p, 'threshold',   .25, @isscalar);
+addParamValue(p, 'threshold',   .2, @isscalar);
+addParamValue(p, 'threshold_sqddx',   10, @isscalar);
 addParamValue(p, 'Q_dx',  1/3, @isscalar);
 
 parse(p, varargin{:});
 %%
 T = numel(p.Results.x);
-t = 1:T;
+t = (1:T)';
 
 xdim = find(size(p.Results.x)>1);
 
@@ -44,42 +45,82 @@ bnd = bnd(dx(bnd)>0);
 bnd = bnd(dx(bnd) > quantile(dx(bnd), p.Results.Q_dx));
 
 bnd = cat(xdim, 1, bnd, numel(ddx));
-sqddx = zeros(size(bnd));
 
-for jj = 1:4
-    logInds = true(size(bnd));
-    
-    for ii = 2:numel(bnd)-1        
-        sqddx(ii) = sum(ddx(bnd(ii-1):bnd(ii+1)).^2);
-        logInds(ii) = sqddx(ii) > p.Results.threshold / 4;
-        
-%         if logInds(ii)
-%             [~, ind] = max( abs(ddx(bnd(ii) + [-1:1:1])));
-%             bnd(ii) = bnd(ii)+ind;
-%         end
+%%
+
+    function sqddx = sum_square_ddx(ddx, bnd)
+        sqddx = zeros(size(bnd));
+        l_midpoint =  [0; floor((bnd(1:end-2) + bnd(2:end-1))/2);0];
+        r_midpoint =  [0; floor((bnd(2:end-1) + bnd(3:end))/2); 0];
+        for ii = 2:numel(bnd)-1
+            %             l_midpoint =  floor((bnd(ii-1) + bnd(ii))/2);
+            %             r_midpoint = floor((bnd(ii) + bnd(ii+1))/2);
+            sqddx(ii) = sum(ddx(l_midpoint(ii):r_midpoint(ii)).^2);
+        end
     end
-    
-%     figure
-%     stem(t(bnd), sqddx, 'rx');
-%     set(gca, 'yscale', 'log')
-%     xlabel('time')
-%     ylabel('local sq acceleration')
-%     
-%     bnd = bnd(logInds);
-%     sqddx = sqddx(logInds);
+
+for jj = 8:-1:1
+    sqddx = sum_square_ddx(ddx, bnd);
+    %     figure
+    %     stem(t(bnd), sqddx, 'rx');
+    %     set(gca, 'yscale', 'log')
+    %     xlabel('time')
+    %     ylabel('local sq acceleration')
+    logInds = sqddx > p.Results.threshold/jj;
+    logInds([1,end]) = true;
+    bnd = bnd(logInds);
 end
 
-logInds = sqddx > p.Results.threshold;
-bnd = bnd(logInds);
-sqddx = sqddx(logInds);
+%%
+large_bnds = bnd( dx(bnd)> 1 );
+auxillary_bnds = min_speed_boundaries(dx, large_bnds);
 
-bnd = cat(xdim, 1, bnd+1, T);
-sqddx = cat(xdim, 0, sqddx, 0);
+bnd( dx(bnd)> 1 ) = auxillary_bnds(:,1);
+bnd = sort([bnd(:); auxillary_bnds(:,2)]);
 
-for ii = 2:numel(bnd)-1   
-    [~, ind] = max( abs(ddx(bnd(ii) + [-floor(p.Results.R/2):1:floor(p.Results.R/2)])));
-    bnd(ii) = bnd(ii)+ind;
-end
+
+sqddx = sum_square_ddx(ddx, bnd);
+bnd = sort([bnd(sqddx > p.Results.threshold_sqddx)-3; bnd(sqddx > p.Results.threshold_sqddx)-5; bnd]);
+bnd = bnd(bnd>=1);
+
+%% add end points
+bnd = cat(xdim, 1, bnd, T);
+bnd = bnd(diff(bnd) > 0);
+sqddx = sum_square_ddx(ddx, bnd);
+
+%% optimization
+
+    function fun= interp_error(ind, z, t, varargin)
+        %% check the input parameters
+        pie = inputParser;
+        pie.KeepUnmatched = true;
+        addRequired(pie, 'ind', @isnumeric);
+        addRequired(pie, 'z', @isnumeric);
+        addRequired(pie, 't', @isnumeric);
+        addOptional(pie, 'interp1', 'pchip', @(x)strcmpi(x, {'linear','pchip', 'spline'}) );
+        parse(pie, ind, z, t, varargin{:});
+        %%
+        
+        if any(ind > numel(z)) || any(ind < 1)
+            fun = Inf;
+            return
+        end
+        ii = sort(round(ind));
+        if any(diff(ii) == 0)
+            fun = Inf;
+            return
+        end
+        [t_impl, z_impl] = interp_implicit(ii, z(ii), pie.Results.interp1);
+        %        z_interp = interp1( ii, z(ii), t);
+        z_interp = interp1( t_impl, z_impl, t, pie.Results.interp1, 'extrap');
+        fun =  nansum( (z - z_interp).^2 );
+    end
+
+heuristic_bnd = bnd;
+
+bnd = sort(round(fminsearch(@(y)interp_error(y, p.Results.x,t), bnd)));
+
+
 % figure
 % stem(t(bnd), sqddx, 'rx');
 % set(gca, 'yscale', 'log')
@@ -88,9 +129,20 @@ end
 
 % logInds(1) = true;
 % logInds(end) = true;
-%% plot speed 
-
+%% plot
 if p.Results.visualize
+    %% plot the control points
+    x = p.Results.x;
+    figure
+    plot(t,x, 'g-')
+    hold all
+    plot(heuristic_bnd, x(heuristic_bnd) ,'bx' );
+    plot(t, interp1(heuristic_bnd, x(heuristic_bnd), t),'b-' );
+    hold all
+    plot(bnd, x(bnd), 'ro' );
+    [t_ , x_] = interp_implicit(bnd, x(bnd));
+    plot(t_, x_, 'r-' );
+    %% plot the derivatives
     nrow = 4;
     figure
     ax(1) = subplot(nrow,1,1);
@@ -99,7 +151,7 @@ if p.Results.visualize
     plot(t(bnd), p.Results.x(bnd), 'r+' );
     xlabel('time')
     ylabel('z')
-
+    
     ax(2) = subplot(nrow,1,2);
     plot(t(1:end-1), dx ); hold all
     plot(t(bnd), dx(min(bnd, numel(dx))), 'rx'  );
@@ -108,7 +160,7 @@ if p.Results.visualize
     plot([1, T], [0, 0], 'k-')
     xlabel('time')
     ylabel('speed')
-
+    
     ax(3) = subplot(nrow,1,3);
     plot(t(2:end-1), ddx );
     hold all
@@ -117,7 +169,7 @@ if p.Results.visualize
     ylabel('acceleration')
     
     ax(4) = subplot(nrow,1,4);
-    stem(t(bnd), sqddx, 'rx');
+    stem(t(bnd), sqddx, 'rx', 'BaseValue', 10^-10);
     set(gca, 'yscale', 'log')
     xlabel('time')
     ylabel('local sq acceleration')
---
+++ matlab_source/min_speed_boundaries.m
@@ -0,0 +1,44 @@
+function auxillary_bnds = min_speed_boundaries(dx, large_bnds)
+
+auxillary_bnds = NaN(numel(large_bnds),2);
+
+for kk = 1:numel( large_bnds )
+    ll = large_bnds(kk);
+    if ll > 1 && ll < numel(dx)-1;
+        %===========================
+        ii = ll;
+        jj = ll-1;
+        if abs(dx(jj)) < abs(dx(ii))
+            flag_wrong_side = false;
+        else
+            flag_wrong_side = true;
+        end
+        while abs(dx(jj)) < abs(dx(ii)) || flag_wrong_side
+            ii = ii - 1;
+            jj = jj - 1;
+            if abs(dx(jj)) < abs(dx(ii))
+                flag_wrong_side = false;
+            end
+        end
+        auxillary_bnds(kk,1) = ii;
+        %===========================
+        ii = ll;
+        jj = ll+1;
+        if abs(dx(jj)) < abs(dx(ii))
+            flag_wrong_side = false;
+        else
+            flag_wrong_side = true;
+        end
+        while abs(dx(jj)) < abs(dx(ii))|| flag_wrong_side
+            ii = ii + 1;
+            jj = jj + 1;
+            if abs(dx(jj)) < abs(dx(ii))
+                flag_wrong_side = false;
+            end
+        end
+        auxillary_bnds(kk,2) = ii;
+        %===========================
+    end
+end
+
+end
