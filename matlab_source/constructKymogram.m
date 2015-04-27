884f81f3bab28acceca5b5894d1c83be0a326700
Dmytro S Lituiev <d.lituiev@gmail.com>
2015-04-27 17:28:56 +0200

    handling of 4D movies (with several colour channels in the 3rd dimension); refactoring

--- matlab_source/constructKymogram.m
+++ matlab_source/constructKymogram.m
@@ -1,13 +1,13 @@
 function [kymoGram, path] = constructKymogram(path, mov, varargin)
 % constructs a kymogram given a movie and the path-ROI
 %
-%Input 
+%Input
 %=====
 % - path   -- the path object:
 %    >- path.x and path.y  --  ROI (detailed point coordinates,
 %                                 with the unit spacing between the points)
 %    >- path.L             --  the length of the path
-% 
+%
 % - mov    -- movie (3D array, uint16)
 % - (optional) interpolation mode  -- a case insensitive string;
 %          possible values:
@@ -17,8 +17,11 @@ function [kymoGram, path] = constructKymogram(path, mov, varargin)
 %
 % TODO: extend to support uint8
 %
-
-[ Y, X, T] = size(mov);
+if ndims(mov) == 3
+    [ Y, X, T] = size(mov);
+elseif ndims(mov) == 4
+    [ Y, X, C, T] = size(mov);
+end
 
 if isempty(varargin)
     kymoMethod = 'l1';
@@ -37,15 +40,16 @@ if isempty(path.L)
     path.L = numel(path.x);
 end
 
+assert( all(path.x > 0) )
+assert( all(path.y > 0) )
+
 switch kymoMethod
     
-    case 'l1'    %== L1 distance 
+    case 'l1'    %== L1 distance
         kymoGram = zeros(path.L, T, class(mov));
-        for tt=1:T
-            indt = sub2ind(size(mov), round(path.y), round(path.x), repmat( tt, [numel(path.y),1]) );
-            kymoGram(:,tt) = mov(indt);
-        end
-        
+        yy = round(path.y);
+        xx = round(path.x);
+        weightMatr = 1;
     case 'l2'    %== L2 distance
         dX = bsxfun(@minus, (1:X)', permute(path.x,[2,3,1]) );
         dY = bsxfun(@minus, 1:Y, permute(path.y,[2,3,1]) );
@@ -58,13 +62,8 @@ switch kymoMethod
             dRt = dR(:, :, ll);
             [ ~ , minDistInd(ll)] = min(dRt(:));
         end
-        [xl, yl] = ind2sub([X, Y], minDistInd);
-        
-        kymoGram = zeros(path.L, T, class(mov));
-        for tt = 1:T
-            indt = sub2ind(size(mov), yl, xl, repmat( tt, [path.L, 1]) );
-            kymoGram(:,tt) = mov(indt);
-        end
+        [xx, yy] = ind2sub([X, Y], minDistInd);
+        weightMatr = 1;
         
     case 'm4'    %== M'_4 interpolation kernel
         if numel(varargin)>1
@@ -82,16 +81,34 @@ switch kymoMethod
         
         weights = single(M4prime(dR, epsilon));
         nzwInds = find(weights); %== non-zero weights' indices
-        [wiX, wiY, wiZ] = ind2sub(size(weights), nzwInds);
-        weightMatr = bsxfun(@times, bsxfun( @eq, (1:path.L)', wiZ' ), weights(nzwInds)');
-               
-        indAll = sub2ind(size(mov), repmat(wiY,[1,T]), repmat(wiX,[1,T]), repmat(1:T, [numel(nzwInds),1]) );
-                
-        kymoGram = weightMatr * single(mov(indAll));
-        
-        if isa(mov,'uint8')
+        [xx, yy, zz] = ind2sub(size(weights), nzwInds);
+        weightMatr = bsxfun(@times, bsxfun( @eq, (1:path.L)', zz' ), weights(nzwInds)');
+end
+
+try
+indt = sub2ind(size(mov), repmat(yy,[1,T,C]), repmat(xx,[1,T,C]), ...
+    repmat( permute(1:C, [1,3,2]), [numel(xx),T,1]) ,...
+    repmat(1:T, [numel(xx),1,C]) );
+catch err
+    fprintf('movie size:\t')
+    disp(size(mov))    
+    fprintf('max(y):\t%u\n', max(yy))
+    fprintf('max(x):\t%u\n', max(xx))
+    rethrow(err)
+end
+
+if strcmpi(kymoMethod, 'm4')
+    mov_pix = single(mov(indt));
+    for cc = C:-1:1
+        kymoGram(:,:,cc) =  weightMatr * mov_pix(:,:,cc);
+    end
+    if isa(mov,'uint8')
         kymoGram = uint8( 2^8 * kymoGram./max(kymoGram(:)) );
-        elseif isa(mov,'uint16')
+    elseif isa(mov,'uint16')
         kymoGram = uint16( 2^16 * kymoGram./max(kymoGram(:)) );
-        end
+    end
+else
+    kymoGram =  mov(indt);
+end
+
 end
